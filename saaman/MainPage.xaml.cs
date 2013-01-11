cd1bceed526f73f0a62d837c50c34a7c084441a8
Somay Jain <somay.jain@gmail.com>
2013-01-12 02:04:12 +0530

    Added synchronisation with Azure(online+ offline)

---
+++ saaman/MainPage.xaml.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using saaman.Resources;
+using System.Collections.ObjectModel;
+
+
+using Microsoft.WindowsAzure.MobileServices;
+using System.Diagnostics;
+
+using Microsoft.Phone.Net.NetworkInformation;
+using Microsoft.Phone.Tasks;
+
+namespace saaman
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        [DataMember(Name = "text")]
+        public string Text { get; set; }
+
+        [DataMember(Name = "complete")]
+        public bool Complete { get; set; }
+    }
+
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // MobileServiceCollectionView implements ICollectionView (useful for databinding to lists) and 
+        // is integrated with your Mobile Service to make it easy to bind your data to the ListView
+ //       private ObservableCollection<TodoItem> items2 = new ObservableCollection<TodoItem>();
+        private ObservableCollection<TodoItem> buffer = new ObservableCollection<TodoItem>();
+        private MobileServiceCollectionView<TodoItem> items;
+        public ObservableCollection<TodoItem> retreive = new ObservableCollection<TodoItem>();
+       
+        public static TodoItem local; 
+        
+        public static MobileServiceClient MobileService;
+        private static IMobileServiceTable<TodoItem> todoTable;
+
+        static bool online = false;
+ 
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            RefreshTodoItems();
+            CheckNetworkAvailability();
+            DeviceNetworkInformation.NetworkAvailabilityChanged += new EventHandler<NetworkNotificationEventArgs>(NetworkChange);
+             
+        }
+        private void NetworkChange(object sender, NetworkNotificationEventArgs e)
+        {
+            CheckNetworkAvailability();
+        }
+        private async void CheckNetworkAvailability()
+        {
+            online = NetworkInterface.NetworkInterfaceType.ToString()!="None";
+       //     SetNetworkIndication();
+            Debug.WriteLine(online);
+            if (online)
+            {
+                MobileService = new MobileServiceClient(
+                     "https://saaman.azure-mobile.net/", 
+                       "BPWiwYHRGotSjuciwiTyBSHwDnZNvj71"
+                );
+                todoTable = MobileService.GetTable<TodoItem>();
+            //    Debug.WriteLine(todoTable);
+            //    XmlTaskService.XmlFile = "buffer.xml";
+
+                buffer = XmlTaskService.GetTasks("buffer.xml");
+                
+                foreach (TodoItem buffitem in buffer)
+                {
+                    Debug.WriteLine(buffitem.Id);
+                    try
+                    {
+                        await todoTable.InsertAsync(buffitem);
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                XmlTaskService.DeleteAllTasks("buffer.xml");
+                buffer.Clear();
+                Debug.WriteLine(buffer.Count);
+            }
+        }
+
+        private async void InsertTodoItem(TodoItem todoItem)
+        {
+            // This code inserts a new TodoItem into the database. When the operation completes
+            // and Mobile Services has assigned an Id, the item is added to the CollectionView
+            Debug.WriteLine(todoItem.Id);
+            local = new TodoItem();
+//            local.Id = retreive.Count + 1;
+            local.Id = todoItem.Id;        
+            local.Text = todoItem.Text;
+            local.Complete = todoItem.Complete;
+
+  //          items2.Add(local);
+
+            if (!online)
+            {
+    //            XmlTaskService.XmlFile = "buffer.xml";
+                XmlTaskService.CreateTask(local, "buffer.xml");
+            }
+ //           XmlTaskService.XmlFile = "tasks.xml";
+            XmlTaskService.CreateTask(local, "tasks.xml");
+            
+
+            RefreshTodoItems();
+    
+            if(online)
+            {
+                await todoTable.InsertAsync(todoItem);
+           //     items.Add(todoItem);
+            }
+            
+        }
+
+        private void RefreshTodoItems()
+        {
+            // This code refreshes the entries in the list view be querying the TodoItems table.
+            // The query excludes completed TodoItems
+//            items = todoTable
+//               .Where(todoItem => todoItem.Complete == false)
+//               .ToCollectionView();
+            retreive = XmlTaskService.GetTasks("tasks.xml");
+            ListItems.ItemsSource = retreive;
+        }
+
+        private async void UpdateCheckedTodoItem(TodoItem item)
+        {
+            // This code takes a freshly completed TodoItem and updates the database. When the MobileService 
+            // responds, the item is removed from the list 
+            await todoTable.UpdateAsync(item);
+            items.Remove(item);
+        }
+
+        private void ButtonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var todoItem = new TodoItem { Text = TodoInput.Text };
+            InsertTodoItem(todoItem);
+        }
+
+        private void CheckBoxComplete_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox cb = (CheckBox)sender;
+            TodoItem item = cb.DataContext as TodoItem;
+            item.Complete = true;
+            UpdateCheckedTodoItem(item);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            RefreshTodoItems();
+        }
+    }
+}
---
+++ saaman/XmlTaskService.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Xml.Linq;
+//using TaskMaster.Abstract;
+//using TaskMaster.Models;
+//using TaskMaster.ViewModels;
+
+namespace saaman
+{
+    public class XmlTaskService 
+    {
+ //       public const string XmlFile = "tasks.xml";
+
+        public static ObservableCollection<TodoItem> GetTasks(string XmlFile)
+        {
+            ObservableCollection<TodoItem> tasks_ret = new ObservableCollection<TodoItem>();
+            var tasks = new List<TodoItem>();
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (store.FileExists(XmlFile))
+                {
+                    //store.DeleteFile(XmlFile);
+                    //XDocument doc = XDocument.Load(store.OpenFile(XmlFile, FileMode.Open));
+                    using (var sr = new StreamReader(new IsolatedStorageFileStream(XmlFile, FileMode.Open, store)))
+                    {
+                        XDocument doc = XDocument.Load(sr);
+                        tasks = (from d in doc.Descendants("task")
+                                 select new TodoItem
+                                 {
+  //                                   Category = (string)d.Attribute("category"),
+                                     Id = (int)d.Element("Id"),
+                                     Text = (string)d.Element("Text"),
+  //                                   CreateDate = (DateTime)d.Element("createdate"),
+  //                                   DueDate = (DateTime)d.Element("duedate"),
+                                     Complete = (bool)d.Element("Complete")
+                                 }).ToList<TodoItem>();
+                    }
+                    foreach (TodoItem item in tasks)
+                        tasks_ret.Add(item);
+
+                }
+            }
+            return tasks_ret;
+        }
+
+        public static bool DeleteAllTasks(string XmlFile)
+        {
+            try
+            {
+                using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    store.DeleteFile(XmlFile);
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool CreateTask(TodoItem t, string XmlFile)
+        {
+
+            var doc = ReadXml(XmlFile);
+            try
+            {
+                if (doc.Element("tasks") != null)
+                {
+                    //this is an append
+                    doc.Element("tasks").Add(
+                        new XElement("task",
+                                     new XElement("Text", t.Text),
+                                     new XElement("Complete", "false"), 
+                                     new XElement("Id", t.Id) 
+                            ));
+                    WriteXml(doc, XmlFile);
+                }
+                else
+                {
+                    doc.Add(
+                           new XElement("tasks",
+                                   new XElement("task",
+                                         new XElement("Text", t.Text),
+                                         new XElement("Complete", "false"),
+                                         new XElement("Id", t.Id) 
+                                   )
+                      ));
+                    WriteXml(doc, XmlFile);
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static XDocument ReadXml(string XmlFile)
+        {
+            var doc = new XDocument();
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                try
+                {
+                    if (store.FileExists(XmlFile))
+                    {
+                        using (var sr = new StreamReader(new IsolatedStorageFileStream(XmlFile, FileMode.OpenOrCreate, store)))
+                        {
+                            doc = XDocument.Load(sr);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+            return doc;
+        }
+
+        private static bool WriteXml(XDocument document, string XmlFile)
+        {
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                try
+                {
+                    using (var sw = new StreamWriter(new IsolatedStorageFileStream(XmlFile, FileMode.Create, store)))
+                    {
+                        sw.Write(document.ToString());
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool MarkTaskComplete(string id, string XmlFile)
+        {
+            XDocument doc = ReadXml(XmlFile);
+
+            foreach (var ele in doc.Descendants())
+            {
+                if (ele != null && (string)ele.Attribute("id") == id)
+                {
+                    ele.SetElementValue("Complete", "true");
+                    break;
+                }
+            }
+            return WriteXml(doc, XmlFile);
+        }
+
+        public static bool DeleteTask(string id, string XmlFile)
+        {
+            XDocument doc = ReadXml(XmlFile);
+
+            foreach (var ele in doc.Descendants())
+            {
+                if (ele != null && (string)ele.Attribute("id") == id)
+                {
+                    ele.Remove();
+                    break;
+                }
+            }
+            return WriteXml(doc, XmlFile);
+        }
+    }
+}
